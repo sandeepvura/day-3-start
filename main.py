39f3addc2585fd287acdc2d330188cb59e8381b2
sandeepvura <>
2021-05-19 11:03:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+# if height > 120:
+#   print("you can ride the roller coaster")
+# else:
+#   print("Sorry you can't ride the roller coaster")
+
+# Note: If the height is exactly 120 it prints the else condition block to print the if conditional block for height 120cm you need to specify is equal to (==) OR (>=) "
+
+if height == 120:
+  print("you can ride the roller coaster")
+else:
+  print("Sorry you can't ride the roller coaster")
